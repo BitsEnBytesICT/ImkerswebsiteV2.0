40db97ef132e5de25589247e80454d53a55e3082
rayan1159 <baskruithof999@gmail.com>
2023-07-12 10:10:02 -0700

    Added login form base design (unfinished)

--- src/layout/login.tsx
+++ src/layout/login.tsx
@@ -1,15 +1,15 @@
 import '../resources/css/App.css';
 import Header from './components/header';
+import Honeycomb from './components/honeycomb';
 
 export const Login = (): JSX.Element => {
     return (
-        <div className='flex flex-col h-screen bg-yellow-700'>
-            <header>
-                <Header/>
-            </header>
-            <div className="login-form">
-                <div className='master-key'>
-                    <input type='text' placeholder='Master sleutel' name="key"/>
+        <div className='admin-login flex flex-col h-screen bg-yellow-700'>
+            <div className="auth">
+                <div className="login-form">
+                    <div className='master-key'>
+                        <input type='text' placeholder='Master sleutel' name="key"/>
+                    </div>
                 </div>
             </div>
         </div>
