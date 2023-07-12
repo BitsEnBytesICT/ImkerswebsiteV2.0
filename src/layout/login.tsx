ecaa0fedd57a3532bbad0196721db0f3cf913005
rayan1159 <baskruithof999@gmail.com>
2023-07-12 12:59:15 -0700

    Almost finished login page, just need to add a login button

--- src/layout/login.tsx
+++ src/layout/login.tsx
@@ -3,12 +3,20 @@ import Header from './components/header';
 import Honeycomb from './components/honeycomb';
 
 export const Login = (): JSX.Element => {
+
+    const login = () => {
+        
+    }
+
     return (
         <div className='admin-login flex flex-col h-screen bg-yellow-700'>
             <div className="auth">
                 <div className="login-form">
                     <div className='master-key'>
                         <input type='text' placeholder='Master sleutel' name="key"/>
+                    </div>
+                    <div>
+                        <button onClick={login} type='button'>Inloggen</button>
                     </div>
                 </div>
             </div>
